881de089d00867166c62454ff959e70b40aebe0e
Chris Zubak-Skees <chriszs@gmail.com>
2018-02-12 18:33:07 -0500

    Update make_ammend_view.sql

--- sql_scripts/make_ammend_view.sql
+++ sql_scripts/make_ammend_view.sql
@@ -3,7 +3,8 @@ CREATE VIEW ld203_amended_filings AS
   (ld203_filings.registrant_id,
     ld203_filings.year,
     ld203_filings.period,
-    ld203_filings.lobbyist_name) ld203_filings.filing_id
+    ld203_filings.lobbyist_name) ld203_filings.filing_id,
+                                 ld203_filings.source_file
   FROM
     ld203_filings
   ORDER BY
@@ -11,4 +12,5 @@ CREATE VIEW ld203_amended_filings AS
     ld203_filings.year,
     ld203_filings.period,
     ld203_filings.lobbyist_name,
+    ld203_filings.source_file DESC,
     ld203_filings.received DESC;
