762be5e256a734b5d078145d66473ad57f0cff4b
Pratheek Rebala <pratheekrebala@gmail.com>
2018-02-12 16:55:26 -0500

    Clean.
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- process.sh
+++ process.sh
@@ -1,11 +1,12 @@
 clear
 
 # Scrape senate page to get latest available files.
-needUpdates=$(node index.js)
+needUpdates=$(node ./lib/index.js)
 
 mkdir -p downloads/
 mkdir -p extracted/
 mkdir -p parsed/
+mkdir -p dist/
 rm -rf parsed/*
 
 process_quarter() {
@@ -14,17 +15,17 @@ process_quarter() {
     echo "Processing: $sourceFile"
 
     echo "Downloading: $sourceFile"
-    wget -q http://soprweb.senate.gov/downloads/$sourceFile -O downloads/$sourceFile
+    #wget -q http://soprweb.senate.gov/downloads/$sourceFile -O downloads/$sourceFile
 
     echo "Extracting: $sourceFile"
-    unzip -o downloads/$sourceFile -d extracted/${sourceFile%%.*}
+    #unzip -o downloads/$sourceFile -d extracted/${sourceFile%%.*}
 
     # Convert each file in extracted .zip to CSV & JSON.
     echo "Parsing: $sourceFile"
     mkdir -p parsed/${sourceFile%%.*}
     for subXMLFile in extracted/${sourceFile%%.*}/*.xml; do
         outputPrefix=extracted/${sourceFile%%.*}
-        node convert.js --output=${outputPrefix/extracted/parsed} --input=$subXMLFile
+        node ./lib/convert.js --output=${outputPrefix/extracted/parsed} --input=$subXMLFile
     done
 
     echo "Finished Processing: $sourceFile"
